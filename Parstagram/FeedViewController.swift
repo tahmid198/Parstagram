cf269231f7df20160ac2b538078d9560aac9c399
Tahmid Zaman <tahmid198@yahoo.com>
2021-03-20 02:03:35 -0400

    Able to display comment text view in app

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -25,8 +25,9 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        
         let query = PFQuery(className: "Posts")
-        query.includeKey("author")
+        query.includeKeys(["author", "comments", "comments.author"]) 
         query.limit = 20
         
         query.findObjectsInBackground(){ (posts, error) in
@@ -38,25 +39,53 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         
     }
 }
+    //Number or rows
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let post = posts[section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        return comments.count+1
+    }
+    
+    //Number of sections
+    func numberOfSections(in tableView: UITableView) -> Int {
         return posts.count
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
         
-        let post = posts[indexPath.row]
-        let user = post["author"] as! PFUser
-        cell.usernameLabel.text = user.username
+        let post = posts[indexPath.section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
+
         
-        cell.captionLabel.text = post["caption"] as! String
+        if indexPath.row == 0 { //Postcell is at 0 row
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+            
+            let user = post["author"] as! PFUser
+            cell.usernameLabel.text = user.username
+            
+            cell.captionLabel.text = post["caption"] as! String
+            
+            let imageFile  = post["image"] as! PFFileObject
+            let urlString = imageFile.url!
+            let url = URL(string: urlString)!
+            
+            cell.photoView.af_setImage(withURL: url)
         
-        let imageFile  = post["image"] as! PFFileObject
-        let urlString = imageFile.url!
-        let url = URL(string: urlString)!
-        
-        cell.photoView.af_setImage(withURL: url)  
-        
-        return cell
+            return cell
+        } else{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell") as! CommentCell
+            
+            let comment =  comments[indexPath.row - 1] //Brings you first comment
+            cell.commentLabel.text = comment["text"] as? String
+            
+            let user = comment["author"] as! PFUser
+            cell.nameLabel.text = user.username
+            
+            
+            
+            
+            return cell
+        }
     }
      
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
